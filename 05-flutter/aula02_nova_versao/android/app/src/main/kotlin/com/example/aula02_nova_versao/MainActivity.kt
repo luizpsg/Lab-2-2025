58bdd6b359e9f3ee3d6a895c225397489460db6a
Luiz Paulo Saud <luiz.saud@sga.pucminas.br>
2025-10-20 22:13:59 -0300

    Add initial Flutter project for aula02_nova_versao
    This commit sets up a new Flutter project named 'aula02_nova_versao' with multi-platform support, including Android, iOS, Linux, macOS, Windows, and Web. It includes essential configuration files, a basic structure for the application, and initial assets such as icons and launch images. The project is ready for further development and customization.

---
+++ 05-flutter/aula02_nova_versao/android/app/src/main/kotlin/com/example/aula02_nova_versao/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.aula02_nova_versao
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- 05-flutter/task_manager/lib/main.dart
+++ 05-flutter/task_manager/lib/main.dart
@@ -6,13 +6,64 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Task Manager',
-      theme: ThemeData(primarySwatch: Colors.blue),
+      title: 'Task Manager Pro',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(255, 61, 168, 114),
+          brightness: Brightness.light,
+        ),
+        useMaterial3: true,
+        cardTheme: const CardThemeData(
+          // ← CardThemeData ao invés de CardTheme
+          elevation: 2,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(12)),
+          ),
+        ),
+        inputDecorationTheme: const InputDecorationTheme(
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(8)),
+          ),
+          filled: true,
+          fillColor: Color(0xFFF5F5F5), // Colors.grey.shade50
+        ),
+      ),
+
+      darkTheme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Color.fromARGB(255, 61, 168, 114),
+          brightness: Brightness.dark,
+        ),
+        useMaterial3: true,
+        textTheme: const TextTheme(
+          bodyLarge: TextStyle(color: Colors.white),
+          bodyMedium: TextStyle(color: Colors.white),
+          bodySmall: TextStyle(color: Colors.white70),
+          titleLarge: TextStyle(color: Colors.white),
+          titleMedium: TextStyle(color: Colors.white),
+          titleSmall: TextStyle(color: Colors.white),
+        ),
+        cardTheme: const CardThemeData(
+          elevation: 2,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(12)),
+          ),
+        ),
+        inputDecorationTheme: const InputDecorationTheme(
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(8)),
+          ),
+          filled: true,
+          fillColor: Color(0xFF121212), // Colors.grey.shade900
+        ),
+      ),
+      themeMode: ThemeMode.dark,
       home: const TaskListScreen(),
     );
   }
